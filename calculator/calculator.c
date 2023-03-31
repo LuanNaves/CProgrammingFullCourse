a14b7037241466cbe7429cd2242628f8e3a809d2
Luan Felipe <106930442+LuanNaves@users.noreply.github.com>
2023-03-31 13:54:25 -0300

    Add files via upload

---
+++ calculator/calculator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+
+    char operator;
+    double num1, num2, result;
+    
+    printf("\nEnter an operator (+ - * /): ");
+    scanf("%c", &operator);
+
+    printf("Enter number 1: ");
+    scanf("%lf", &num1);
+
+    printf("Enter number 2: ");
+    scanf("%lf", &num2);
+
+    switch (operator){
+    case '+':
+        result = num1 + num2;
+        printf("Result: %.2lf", result);
+        break;
+    case '-':
+        result = num1 - num2;
+        printf("Result: %.2lf", result);
+        break;
+    case '*':
+        result = num1 * num2;
+        printf("Result: %.2lf", result);;
+        break;
+    case '/':
+        result = num1 / num2;
+        printf("Result: %.2lf", result);
+        break;
+    default:
+        printf("Result: %.2lf", result);
+        break;
+    }   
+
+    return 0;
+}
